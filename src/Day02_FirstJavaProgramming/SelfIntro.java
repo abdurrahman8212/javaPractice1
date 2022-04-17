28367377b70f0a3b71cc55618fd6c1eebd011b8f
Abdurrahman <okur61453@gmail.com>
2022-04-17 16:25:29 +0300

    Day01_SelfIntro

---
+++ src/Day02_FirstJavaProgramming/SelfIntro.java
@@ -0,0 +1,16 @@
+package Day02_FirstJavaProgramming;
+
+public class SelfIntro {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Cydeo");
+        System.out.println("My name is : Abdurrahman");
+        System.out.println("I am from : Turkey");
+        System.out.println("I am currently living in : Bursa");
+        System.out.println("My favorite Book is : The White Tiger");
+        System.out.println("My favorite Tv Show is : BBC Documentary");
+        System.out.println("My favorite Sport is : Basketball");
+        System.out.println("My favorite Job is : Developer");
+    }
+}
