97401f3063dec7f430f1248f0118e55e2a4062c9
Abdurrahman <okur61453@gmail.com>
2022-04-17 16:29:34 +0300

    Day01_EmployeeInfo

---
+++ src/Day02_FirstJavaProgramming/EmployeeInfo.java
@@ -0,0 +1,13 @@
+package Day02_FirstJavaProgramming;
+
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Abdurrahman Okur");
+        System.out.println("Cydeo");
+        System.out.println("zx120");
+        System.out.println("Instructor");
+        System.out.println("2000$");
+    }
+}
