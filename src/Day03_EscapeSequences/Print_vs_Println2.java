02df84c1f29c39f36d6261289e8cc6428b81db86
Abdurrahman <okur61453@gmail.com>
2022-04-17 18:05:44 +0300

    Day02_Print_vs_Println2

---
+++ src/Day03_EscapeSequences/Print_vs_Println2.java
@@ -0,0 +1,22 @@
+package Day03_EscapeSequences;
+
+public class Print_vs_Println2 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Knock Knock");
+        System.out.println("Who is this?");
+
+        System.out.println("This is Java");
+
+        System.out.println("----------------------");
+        System.out.print("Knock Knock");
+        System.out.print("Who is this?");
+        System.out.println("--------------------------");
+        System.out.println(" Hello everyone, today we learn escape Sequence and tomorrow we will learn Variables");
+        System.out.println("--------------------------");
+        System.out.print("Hello everyone,today we learn");
+        System.out.print("Escape Sequences and tomorrow");
+        System.out.print("we will learn Variables");
+    }
+}
