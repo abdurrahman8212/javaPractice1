28b39bbb8128cbb8f396226c6df5b80c5ace1738
Abdurrahman <okur61453@gmail.com>
2022-04-17 17:19:48 +0300

    Day02_GasReceipt

---
+++ src/Day03_EscapeSequences/GasReceipt.java
@@ -0,0 +1,17 @@
+package Day03_EscapeSequences;
+
+public class GasReceipt {
+
+    public static void main(String[] args) {
+
+        System.out.println("\t\tMCLEAN STORE");
+        System.out.println();
+        System.out.println("\t2021-09-20\t09:25PM");
+        System.out.println();
+        System.out.println("Gallons:\t\t11.840");
+        System.out.println("Price/gallon:\t$2.089");
+        System.out.println();
+        System.out.println("Fuel Total:\t$ 24.734");
+    }
+
+}
