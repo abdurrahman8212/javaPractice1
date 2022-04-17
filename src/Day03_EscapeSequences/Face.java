e888b0113143a7aec1e644881f41202cdfc900d2
Abdurrahman <okur61453@gmail.com>
2022-04-17 17:32:06 +0300

    Day02_Face

---
+++ src/Day03_EscapeSequences/Face.java
@@ -0,0 +1,13 @@
+package Day03_EscapeSequences;
+
+public class Face {
+
+    public static void main(String[] args) {
+
+        System.out.println("+\"\t\"\t\"\t\"+");
+        System.out.println("[|   o   o  |]");
+        System.out.println(" |     ^    |");
+        System.out.println(" |    '_'   |");
+        System.out.println("+ - - - - - +");
+    }
+}
