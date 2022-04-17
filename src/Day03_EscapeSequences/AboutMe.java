a7da9c554aa2c2a5402dd437f7009ba93b22cb4b
Abdurrahman <okur61453@gmail.com>
2022-04-17 17:42:35 +0300

    Day02_AboutMe

---
+++ src/Day03_EscapeSequences/AboutMe.java
@@ -0,0 +1,12 @@
+package Day03_EscapeSequences;
+
+public class AboutMe {
+
+    public static void main(String[] args) {
+
+        System.out.println("My favorite music: Classics");
+        System.out.println("My favorite book: The White Tiger");
+        System.out.println("My favorite quote: Be Positive ");
+        System.out.println("My favorite TV Show: Ibo show ");
+    }
+}
