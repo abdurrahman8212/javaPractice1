96ee5a7709c29a2bda1c49e19da8a9203d3676a1
Abdurrahman <okur61453@gmail.com>
2022-04-17 17:37:46 +0300

    Day02_PersonalInfo

---
+++ src/Day03_EscapeSequences/PersonalInfo.java
@@ -0,0 +1,18 @@
+package Day03_EscapeSequences;
+
+public class PersonalInfo {
+
+    public static void main(String[] args) {
+
+        System.out.println("name: Abdurrahman");
+        System.out.println("gender: M");
+        System.out.println("date of birth: 11.12.1982");
+        System.out.println("address:Bursa/Turkey");
+        System.out.println("phone number:5366612216");
+        System.out.println("ssn:9365218770");
+
+
+    }
+
+
+}
