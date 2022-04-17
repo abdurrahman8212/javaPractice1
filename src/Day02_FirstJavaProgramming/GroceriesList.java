515c9bd7a022e75dc921911a3f9e75fe07123ebf
Abdurrahman <okur61453@gmail.com>
2022-04-17 16:35:04 +0300

    Day01_Groceries

---
+++ src/Day02_FirstJavaProgramming/GroceriesList.java
@@ -0,0 +1,22 @@
+package Day02_FirstJavaProgramming;
+
+public class GroceriesList {
+
+    public static void main(String[] args) {
+
+        System.out.println("Potatoes");
+        System.out.println("12 tl");
+        System.out.println("Tomatoes");
+        System.out.println("4,95 tl");
+        System.out.println("Cucumber");
+        System.out.println("12 tl");
+        System.out.println("Pepper");
+        System.out.println("25 tl");
+        System.out.println("Cabbage");
+        System.out.println("9 tl");
+        System.out.println("Spinach");
+        System.out.println("8 tl");
+
+
+    }
+}
