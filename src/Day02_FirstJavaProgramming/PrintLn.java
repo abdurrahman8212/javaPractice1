7a9643f437cf7b80985465ed7e6976c40f1463cc
Abdurrahman <okur61453@gmail.com>
2022-04-17 16:12:10 +0300

    Day01_Println

--- src/Day02_FirstJavaProgramming/PrintLn.java
+++ src/Day02_FirstJavaProgramming/PrintLn.java
@@ -4,6 +4,7 @@ public class PrintLn {
 
     public static void main(String[] args) {
 
+
         System.out.println("one");
         System.out.println("two");
         System.out.println("three");
