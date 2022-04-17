9eacc251d39f31b6928765c2b96f8e7af0b70ca8
Abdurrahman <okur61453@gmail.com>
2022-04-17 17:56:00 +0300

    Day02_Diamond2

---
+++ src/Day03_EscapeSequences/Diamond2.java
@@ -0,0 +1,20 @@
+package Day03_EscapeSequences;
+
+public class Diamond2 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("     ^ ");
+        System.out.println("   / * \\ ");
+        System.out.println("  / * * \\ ");
+        System.out.println(" / * * * \\");
+        System.out.println("/         \\");
+        System.out.println("-----------");
+        System.out.println("\\ * * *  /");
+        System.out.println(" \\ * *  /");
+        System.out.println("  \\ *  /");
+        System.out.println("   \\  / ");
+        System.out.println("     V ");
+    }
+}
