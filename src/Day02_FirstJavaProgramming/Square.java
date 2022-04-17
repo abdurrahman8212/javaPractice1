6b3494548966303c69b3c3e26ea2bcbfbf031fd3
Abdurrahman <okur61453@gmail.com>
2022-04-17 16:17:37 +0300

    Day01_Square

--- src/Day02_FirstJavaProgramming/Square.java
+++ src/Day02_FirstJavaProgramming/Square.java
@@ -4,6 +4,14 @@ public class Square {
 
     public static void main(String[] args) {
 
-
+        System.out.println("* * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println("* * * * * * * *");
     }
 }
